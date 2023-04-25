28885db194d5f71752db2f0f8d20ea63b13aa822
Sergio <sergio.skate89@gmail.com>
2023-04-25 17:40:02 -0600

    Finished
    Se quito el Login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 import { MovieComponent } from './movie/movie.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'movie/:type/:id', component: MovieComponent },
